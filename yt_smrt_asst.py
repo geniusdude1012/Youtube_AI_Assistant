2a1ad65ed81d911dc7b02e3620a07416ece995e4
saujanya <shresthasoujan@gmail.com>
2026-03-11 22:28:49 +0545

    Added data ingestion, text splitter and retriever

--- yt_smrt_asst.py
+++ yt_smrt_asst.py
@@ -16,4 +16,36 @@ model=ChatOpenAI()
 #Create an embedding model
 embeddings=OpenAIEmbeddings()
 
+#Step 1a: Indexing(Data Ingestion)
+video_id="6M5VXKLf4D4"
+
+ytt_api=YouTubeTranscriptApi()
+
+try:
+    
+    transcript_list=ytt_api.fetch(video_id,languages=["en"])
+    
+    #Convert into plain text
+    transcript="".join(chunk.text for chunk in transcript_list)
+    print(transcript)
+
+    
+except TranscriptsDisabled:
+    print("No captions available for this video")
+    
+#Step 1b: Text Splitting
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+chunks=splitter.create_documents(transcript)
+print(len(chunks))
+
+#Step 1c & 1d : Embedding Generation and store in vector store
+embeddings=OpenAIEmbeddings()
+vector_store=FAISS.from_documents(chunks,embeddings)
+
+# print(vector_store.index_to_docstore_id)
+
+#Step 2: Retreival
+retriever=vector_store.as_retriever(search_type="similarity",search_kwargs={"k":4})
+result=retriever.invoke("why is deep learning important for medical care")
+print(result)
 
