34f5995948a8ec8558842ab0837b23104ac973ed
saujanya <shresthasoujan@gmail.com>
2026-03-12 09:19:33 +0545

    Added the final prompt with the context and query

--- yt_smrt_asst.py
+++ yt_smrt_asst.py
@@ -46,6 +46,33 @@ vector_store=FAISS.from_documents(chunks,embeddings)
 
 #Step 2: Retreival
 retriever=vector_store.as_retriever(search_type="similarity",search_kwargs={"k":4})
-result=retriever.invoke("why is deep learning important for medical care")
-print(result)
+# result=retriever.invoke("why is deep learning important for medical care")
+# print(result)
+
+
+#Step 3: Augmentation
+prompt=PromptTemplate(
+    template="""
+    You are an helpful assistant.
+    Answer only from provided transcript context.
+    
+    If the context is insufficent, just say you don't know.
+    
+    {context}
+    Question:{question}
+    """ ,
+    input_variables=['context','question']
+)
+
+question="why is deep learning important for medical care"
+retreived_docs=retriever.invoke(question)
+
+context_text="\n\n".join(doc.page_content for doc in retreived_docs)
+
+#Creat the final prompt from context obtained and question
+final_prompt=prompt.invoke({'context':context_text,'question':question})
+
+#Step 4: Generation
+
+
 
